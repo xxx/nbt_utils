9a93c5c03785a159cc3a55695efe8bce9b45e532
mpd <mpd@stipple.com>
2015-03-09 15:08:52 -0700

    update NBTUtils::File to handle reading and writing uncompressed files [resolve #2]

--- lib/nbt_utils/file.rb
+++ lib/nbt_utils/file.rb
@@ -5,9 +5,21 @@ module NBTUtils
     end
 
     def read(path = @path)
-      Zlib::GzipReader.open(path) do |f|
-        @content = StringIO.new(f.read)
+      # Zlib does not provide a way to test a file for compressed, and I'm
+      # not going to tool around with magic numbers, so I guess we use
+      # exceptions for flow control.
+      begin
+        Zlib::GzipReader.open(path) do |f|
+          @content = StringIO.new(f.read)
+        end
+        @compressed = true
+      rescue Zlib::GzipFile::Error
+        ::File.open(path) do |f|
+          @content = StringIO.new(f.read)
+        end
+        @compressed = false
       end
+
 
       last_byte = @content.read(1).bytes.first
       klass = NBTUtils::Tag.tag_type_to_class(last_byte)
@@ -15,9 +27,15 @@ module NBTUtils
       @tag = klass.new(@content, true)
     end
 
-    def write(path = @path, tag = @tag)
-      Zlib::GzipWriter.open(path) do |gz|
-        gz.write tag.to_nbt_string
+    def write(path = @path, tag = @tag, compressed = @compressed)
+      if compressed
+        Zlib::GzipWriter.open(path) do |gz|
+          gz.write tag.to_nbt_string
+        end
+      else
+        ::File.open(path, 'w') do |f|
+          f.write tag.to_nbt_string
+        end
       end
     end
   end
--- nbt_utils.gemspec
+++ nbt_utils.gemspec
@@ -14,7 +14,7 @@ Gem::Specification.new do |s|
   s.required_rubygems_version = ">= 1.3.6"
 
   s.add_dependency "bindata", "~> 1.3"
-  s.add_development_dependency "bundler", ">= 1.0.0"
+  s.add_development_dependency "bundler", "~> 1"
 
   s.required_ruby_version = ">= 1.9"
 
