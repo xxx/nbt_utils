612e9e520713b2591f666b821b2dcf0037146a8e
mpd <mpd@jesters-court.net>
2022-06-07 22:32:13 -0700

    clean this wonky interface up a bit. it's still bad.

--- lib/nbt_utils/file.rb
+++ lib/nbt_utils/file.rb
@@ -2,39 +2,39 @@
 
 module NBTUtils
   class File
+    attr_reader :path, :content, :compressed, :tag
+
     def initialize(path = nil)
       @path = path
     end
 
-    def read(path = @path)
+    def read(read_path = path)
       # Zlib does not provide a way to test a file for compressed, and I'm
       # not going to tool around with magic numbers, so I guess we use
       # exceptions for flow control.
       begin
-        Zlib::GzipReader.open(path) do |f|
+        Zlib::GzipReader.open(read_path) do |f|
           @content = StringIO.new(f.read)
         end
         @compressed = true
       rescue Zlib::GzipFile::Error
-        ::File.open(path) do |f|
-          @content = StringIO.new(f.read)
-        end
+        @content = StringIO.new(::File.read(read_path))
         @compressed = false
       end
 
-      last_byte = @content.read(1).bytes.first
+      last_byte = content.read(1).bytes.first
       klass = NBTUtils::Tag.tag_type_to_class(last_byte)
 
-      @tag = klass.new(@content, named: true)
+      @tag = klass.new(content, named: true)
     end
 
-    def write(path = @path, tag = @tag, compressed = @compressed)
-      if compressed
-        Zlib::GzipWriter.open(path) do |gz|
-          gz.write tag.to_nbt_string
+    def write(write_path = path, write_tag = tag, write_compressed = compressed)
+      if write_compressed
+        Zlib::GzipWriter.open(write_path) do |gz|
+          gz.write write_tag.to_nbt_string
         end
       else
-        ::File.write(path, tag.to_nbt_string)
+        ::File.write(write_path, write_tag.to_nbt_string)
       end
     end
   end
